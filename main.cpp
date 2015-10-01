8c38fb8a36aeeef377925cb794f38d85eedc2947
isabelljansson <janssonisabell@gmail.com>
2015-10-01 11:44:51 +0200

    start up

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <lib/glm/glm/glm.hpp>
+
+
+using namespace std;
+
+int main()
+{
+	//Create a cornell box
+
+	//Create some objects
+
+	//Create light sources
+
+	//Set up camera
+
+	//Monte Carlo ray tracing
+
+	//View image/Save image
+
+
+	return 0;
+}
